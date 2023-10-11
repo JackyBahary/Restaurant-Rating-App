0d66d40f0d16882d66bee5db86e64e8f4b948ce1
JackyBahary <jackyrichiebahary@gmail.com>
2023-10-11 14:01:49 +1100

    Create ProtectedRoute.ts
    Create ProtecRoute to disable user from manually going to webpages without being logged in (WIP)

---
+++ client/src/components/ProtectedRoute.ts
@@ -0,0 +1,12 @@
+import { useContext } from "react";
+import { useNavigate } from "react-router-dom";
+import { AuthContext } from "../contexts/AuthContext";
+
+const PrivateWrapper = ({ children }: { children: any }) => {
+  const navigate = useNavigate();
+  const { email, loggedIn } = useContext(AuthContext);
+  console.log("Email: " + email);
+  return email == "" ? children : navigate('/');
+};
+
+export default PrivateWrapper
